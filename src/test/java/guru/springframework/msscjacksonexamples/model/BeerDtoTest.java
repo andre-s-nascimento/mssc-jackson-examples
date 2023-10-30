34607625a2f4b40b25a47e9e1db197ad5150ce96
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-10-30 12:09:20 -0300

    Após Aula 109

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -0,0 +1,35 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+@JsonTest
+class BeerDtoTest extends BaseTest {
+
+  @Autowired ObjectMapper objectMapper;
+
+  @Test
+  void testSerializeDto() throws JsonProcessingException {
+    BeerDto beerDto = getDto();
+
+    String jsonString = objectMapper.writeValueAsString(beerDto);
+
+    System.out.println(jsonString);
+  }
+
+  @Test
+  void testDeserializeDto() throws IOException {
+    BeerDto beerDto = getDto();
+
+    String json =
+        "{\"id\":\"439e3c75-96eb-46ec-afe6-5bd0b688b7e7\",\"beerName\":\"Beer Name\",\"beerStyle\":\"ALE\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2023-10-30T11:44:45.579481-03:00\",\"lastUpdatedDate\":\"2023-10-30T11:44:45.579567-03:00\"}";
+
+    BeerDto dto = objectMapper.readValue(json, BeerDto.class);
+
+    System.out.println(dto);
+  }
+}
